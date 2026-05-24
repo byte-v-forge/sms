8208f3b31b69f19bbb09b4aa9cf129140198c343
pood1e <codingpood1e@gmail.com>
2026-05-25 00:46:18 +0800

    refactor sms id fallback

--- internal/app/id.go
+++ internal/app/id.go
@@ -2,15 +2,22 @@ package app
 
 import (
 	"crypto/rand"
+	"encoding/binary"
 	"encoding/hex"
+	"sync/atomic"
+	"time"
 )
 
 type RandomIDGenerator struct{}
 
+var fallbackIDCounter atomic.Uint64
+
 func (RandomIDGenerator) NewID(prefix string) string {
 	var b [12]byte
-	if _, err := rand.Read(b[:]); err != nil {
-		panic(err)
+	if _, err := rand.Read(b[:]); err == nil {
+		return prefix + hex.EncodeToString(b[:])
 	}
+	binary.BigEndian.PutUint64(b[:8], uint64(time.Now().UnixNano()))
+	binary.BigEndian.PutUint32(b[8:], uint32(fallbackIDCounter.Add(1)))
 	return prefix + hex.EncodeToString(b[:])
 }
