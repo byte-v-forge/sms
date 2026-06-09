181eaf9ca35eaea8c4a7c57709397ee2540b5292
pood1e <codingpood1e@gmail.com>
2026-06-09 09:38:57 +0800

    Deduplicate SMS public route target mapping

--- internal/app/order_route_from_public.go
+++ internal/app/order_route_from_public.go
@@ -18,22 +18,7 @@ func RouteFromPublicAcquireParams(params *smsv1.SmsNumberAcquireParams) core.Rou
 		MaxPrice:           moneyFromProto(params.GetMaxPrice()),
 		FailurePolicy:      routeFailurePolicyFromProto(params.GetRouteFailurePolicy()),
 	}
-	if refRoute := RouteFromPublicOfferRef(params.GetOfferRef()); refRoute.ProviderKey != "" {
-		route.ProviderKey = refRoute.ProviderKey
-		route.UpstreamServiceKey = refRoute.UpstreamServiceKey
-		route.ProviderCountryID = refRoute.ProviderCountryID
-		route.UpstreamProviderID = refRoute.UpstreamProviderID
-		if route.ApplicationKey == "" {
-			route.ApplicationKey = refRoute.ApplicationKey
-		}
-		if route.CountryISO2 == "" {
-			route.CountryISO2 = refRoute.CountryISO2
-		}
-		if route.CountryCallingCode == "" {
-			route.CountryCallingCode = refRoute.CountryCallingCode
-		}
-	}
-	return route
+	return mergeOfferRefRoute(route, RouteFromPublicOfferRef(params.GetOfferRef()))
 }
 
 func RouteFromPublicOfferRef(ref *smsv1.SmsOfferRef) core.Route {
@@ -43,21 +28,45 @@ func RouteFromPublicOfferRef(ref *smsv1.SmsOfferRef) core.Route {
 	route := core.Route{ProviderKey: normalizeProviderKey(ref.GetProviderKey())}
 	target := ref.GetTarget()
 	if target != nil {
-		if route.ApplicationKey == "" {
-			route.ApplicationKey = routeText(target.GetApplicationKey())
-		}
-		if route.CountryISO2 == "" {
-			route.CountryISO2 = routeCountryISO2(target.GetCountryIso2())
-		}
-		if route.CountryCallingCode == "" {
-			route.CountryCallingCode = routeCallingCode(target.GetCountryCallingCode())
-		}
+		route = fillRouteTargetDefaults(route, routeFromPublicTarget(target))
 	}
 	routeRef := ref.GetRouteRef()
 	if routeRef != nil {
 		route.UpstreamServiceKey = routeText(routeRef.GetUpstreamServiceKey())
 		route.ProviderCountryID = routeText(routeRef.GetProviderCountryId())
 		route.UpstreamProviderID = routeText(routeRef.GetUpstreamProviderId())
+	}
+	return route
+}
+
+func mergeOfferRefRoute(route core.Route, refRoute core.Route) core.Route {
+	if refRoute.ProviderKey == "" {
+		return route
+	}
+	route.ProviderKey = refRoute.ProviderKey
+	route.UpstreamServiceKey = refRoute.UpstreamServiceKey
+	route.ProviderCountryID = refRoute.ProviderCountryID
+	route.UpstreamProviderID = refRoute.UpstreamProviderID
+	return fillRouteTargetDefaults(route, refRoute)
+}
+
+func routeFromPublicTarget(target *smsv1.SmsTarget) core.Route {
+	return core.Route{
+		ApplicationKey:     routeText(target.GetApplicationKey()),
+		CountryISO2:        routeCountryISO2(target.GetCountryIso2()),
+		CountryCallingCode: routeCallingCode(target.GetCountryCallingCode()),
+	}
+}
+
+func fillRouteTargetDefaults(route core.Route, defaults core.Route) core.Route {
+	if route.ApplicationKey == "" {
+		route.ApplicationKey = defaults.ApplicationKey
+	}
+	if route.CountryISO2 == "" {
+		route.CountryISO2 = defaults.CountryISO2
+	}
+	if route.CountryCallingCode == "" {
+		route.CountryCallingCode = defaults.CountryCallingCode
 	}
 	return route
 }
