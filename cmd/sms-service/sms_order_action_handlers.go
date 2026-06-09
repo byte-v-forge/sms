3ac825c0812fc18893a4f198f1120fd955b95bdd
pood1e <codingpood1e@gmail.com>
2026-06-09 11:15:00 +0800

    Deduplicate SMS order action handling

--- cmd/sms-service/sms_order_action_handlers.go
+++ cmd/sms-service/sms_order_action_handlers.go
@@ -6,6 +6,7 @@ import (
 
 	smsv1 "github.com/byte-v-forge/sms/gen/go/byte/v/forge/contracts/sms/v1"
 	smsinternalv1 "github.com/byte-v-forge/sms/gen/go/byte/v/forge/sms/private/v1"
+	"google.golang.org/protobuf/proto"
 )
 
 func (s *dashboardServer) handleSMSOrder(w http.ResponseWriter, r *http.Request) {
@@ -32,63 +33,59 @@ func (s *dashboardServer) handleSMSOrder(w http.ResponseWriter, r *http.Request)
 }
 
 func (s *dashboardServer) handleSMSOrderMarkMessageSent(w http.ResponseWriter, r *http.Request, orderID string) {
-	var req smsv1.MarkMessageSentRequest
-	if err := readProtoJSON(r, &req); err != nil {
-		writeError(w, http.StatusBadRequest, err)
-		return
-	}
-	req.OrderId = orderID
-	resp, err := s.smsOrderClient.MarkMessageSent(r.Context(), &req)
-	if err != nil {
-		writeError(w, http.StatusBadGateway, err)
-		return
-	}
-	writeProtoJSON(w, http.StatusOK, resp)
+	req := &smsv1.MarkMessageSentRequest{}
+	s.handleSMSOrderProtoAction(w, r, req, func() {
+		req.OrderId = orderID
+	}, func() (proto.Message, error) {
+		return s.smsOrderClient.MarkMessageSent(r.Context(), req)
+	})
 }
 
 func (s *dashboardServer) handleSMSOrderRequestAdditionalCode(w http.ResponseWriter, r *http.Request, orderID string) {
-	var req smsv1.RequestAdditionalCodeRequest
-	if err := readProtoJSON(r, &req); err != nil {
-		writeError(w, http.StatusBadRequest, err)
-		return
-	}
-	req.OrderId = orderID
-	resp, err := s.smsOrderClient.RequestAdditionalCode(r.Context(), &req)
-	if err != nil {
-		writeError(w, http.StatusBadGateway, err)
-		return
-	}
-	writeProtoJSON(w, http.StatusOK, resp)
+	req := &smsv1.RequestAdditionalCodeRequest{}
+	s.handleSMSOrderProtoAction(w, r, req, func() {
+		req.OrderId = orderID
+	}, func() (proto.Message, error) {
+		return s.smsOrderClient.RequestAdditionalCode(r.Context(), req)
+	})
 }
 
 func (s *dashboardServer) handleSMSOrderComplete(w http.ResponseWriter, r *http.Request, orderID string) {
-	var req smsv1.CompleteOrderRequest
-	if err := readProtoJSON(r, &req); err != nil {
-		writeError(w, http.StatusBadRequest, err)
-		return
-	}
-	req.OrderId = orderID
-	resp, err := s.smsOrderClient.CompleteOrder(r.Context(), &req)
-	if err != nil {
-		writeError(w, http.StatusBadGateway, err)
-		return
-	}
-	writeProtoJSON(w, http.StatusOK, resp)
+	req := &smsv1.CompleteOrderRequest{}
+	s.handleSMSOrderProtoAction(w, r, req, func() {
+		req.OrderId = orderID
+	}, func() (proto.Message, error) {
+		return s.smsOrderClient.CompleteOrder(r.Context(), req)
+	})
 }
 
 func (s *dashboardServer) handleSMSOrderCancel(w http.ResponseWriter, r *http.Request, orderID string) {
-	var req smsinternalv1.CancelProviderOrderRequest
-	if err := readProtoJSON(r, &req); err != nil {
+	req := &smsinternalv1.CancelProviderOrderRequest{}
+	if err := readProtoJSON(r, req); err != nil {
 		writeError(w, http.StatusBadRequest, err)
 		return
 	}
 	req.OrderId = orderID
-	resp, err := s.smsAdminClient.CancelOrder(r.Context(), &req)
+	resp, err := s.smsAdminClient.CancelOrder(r.Context(), req)
 	if err != nil {
 		writeError(w, http.StatusBadGateway, err)
 		return
 	}
 	if writeProviderError(w, resp.GetError()) {
+		return
+	}
+	writeProtoJSON(w, http.StatusOK, resp)
+}
+
+func (s *dashboardServer) handleSMSOrderProtoAction(w http.ResponseWriter, r *http.Request, req proto.Message, setOrderID func(), call func() (proto.Message, error)) {
+	if err := readProtoJSON(r, req); err != nil {
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+	setOrderID()
+	resp, err := call()
+	if err != nil {
+		writeError(w, http.StatusBadGateway, err)
 		return
 	}
 	writeProtoJSON(w, http.StatusOK, resp)
